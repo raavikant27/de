d3d2a7e30f9838b60d53b1e296ed9028535377cb
raavikant27 <sravikant302@gmail.com>
2024-06-14 12:47:40 +0530

    first commit

--- src/Components/EditForm.js
+++ src/Components/EditForm.js
@@ -1,18 +1,25 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-export const EditForm = ({editTodo, task}) => {
-    const [value, setValue] = useState(task.task)
+export const EditForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState(task.task);
 
-    const handleSubmit = (e) => {
-      // prevent default action
-        e.preventDefault();
-        // edit todo
-        editTodo(value, task.id);
-      };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    editTodo(value, task.id);
+  };
+
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
-    <input  type="text" value={value} onChange={(e) => setValue(e.target.value)} className='formInput' placeholder='Update task' />
-    <button type="submit" className='todo-btn'>update Task</button>
-  </form>
-  )
-}
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="formInput"
+        placeholder="Update task"
+      />
+      <button type="submit" className="todo-btn">
+        Update Task
+      </button>
+    </form>
+  );
+};
--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,52 +1,39 @@
-import React from 'react'
-import { useState } from 'react'
-import {v4 as uuidv4} from 'uuid'
-import { EditForm } from './EditForm'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-uuidv4();
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { EditForm } from "./EditForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function ToDoList() {
   const [tasks, setTasks] = useState([]);
-  const [newTask,setNewTask] =useState("");
-  
-  function handleInputChange(event){
+  const [newTask, setNewTask] = useState("");
+
+  const handleInputChange = (event) => {
     setNewTask(event.target.value);
-  }
+  };
 
-  function addTask(event){
-    event.preventDefault(); 
+  const addTask = (event) => {
+    event.preventDefault();
     if (newTask.trim() !== "") {
-      setTasks([...tasks, {id: uuidv4() , task: newTask, completed: false , isEditing: false}]);
-      setNewTask(""); 
-      console.log(tasks)
+      setTasks([
+        ...tasks,
+        { id: uuidv4(), task: newTask, completed: false, isEditing: false },
+      ]);
+      setNewTask("");
     }
-  }
+  };
 
-  function deleteTask(Id){
-    setTasks(tasks.filter(task => task.id !== Id));
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
-  }
-  // const editTask = (item,id) => {
-  //   // Function to edit task
-  //   const newTask = prompt(item.task);
-  //   if (newTask) {
-  //     setTasks(
-  //       tasks.map((task) =>
-  //         task.id === id ? { ...task, task: newTask } : task
-  //       )
-  //     );
-  //   }
-  // };
- 
   const editTodo = (id) => {
     setTasks(
       tasks.map((todo) =>
         todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
       )
     );
-  } 
+  };
 
   const editTask = (task, id) => {
     setTasks(
@@ -55,53 +42,62 @@ function ToDoList() {
       )
     );
   };
-  
 
-
-  const toggleComplete=(id) =>{
-    setTasks(tasks.map((item)=>
-    item.id===id?{...item,completed: !item.completed } : item))
-  }
+  const toggleComplete = (id) => {
+    setTasks(
+      tasks.map((item) =>
+        item.id === id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  };
 
   return (
-    <div className='to-do-list'>
-        <h1>To Do List   </h1>
-        <div>
-          <form className='toDoForm' onSubmit={addTask}>
-            
-            <input 
-            className='formInput'
-            type="text" 
-            placeholder='Enter the task...'  
+    <div className="to-do-list">
+      <h1>To Do List</h1>
+      <div>
+        <form className="toDoForm" onSubmit={addTask}>
+          <input
+            className="formInput"
+            type="text"
+            placeholder="Enter the task..."
             value={newTask}
-            onChange={(handleInputChange)}
-            />
-            <button className="sub-btn" type='submit'>Add Task</button>
-          </form>
-        </div>
-       <div className='displayToDo'>
-          {tasks.map((item) =>(
-             item.isEditing ? (
-              <EditForm editTodo={editTask} task={item} />
-            ) :
-          (  
-          <div className='display'>
-            
-            <p className={`${item.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(item.id)}>{item.task}</p>
-            <div className='icon'>
-        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={()=>editTodo(item.id)}  />
-        <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={ ()=>deleteTask(item.id)} />
-        </div>
-        </div>
-        
-      )
-    ))}
-        </div>
-        
-        
+            onChange={handleInputChange}
+          />
+          <button className="sub-btn" type="submit">
+            Add Task
+          </button>
+        </form>
+      </div>
+      <div className="displayToDo">
+        {tasks.map((item) =>
+          item.isEditing ? (
+            <EditForm key={item.id} editTodo={editTask} task={item} />
+          ) : (
+            <div className="display" key={item.id}>
+              <p
+                className={`${item.completed ? "completed" : "incompleted"}`}
+                onClick={() => toggleComplete(item.id)}
+              >
+                {item.task}
+              </p>
+              <div className="icon">
+                <FontAwesomeIcon
+                  className="edit-icon"
+                  icon={faPenToSquare}
+                  onClick={() => editTodo(item.id)}
+                />
+                <FontAwesomeIcon
+                  className="delete-icon"
+                  icon={faTrash}
+                  onClick={() => deleteTask(item.id)}
+                />
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default ToDoList
+export default ToDoList;
